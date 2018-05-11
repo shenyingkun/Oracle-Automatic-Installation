2e08930b71d3c348c84077fd89fc4e2ce1d8bb6c
Cobbler <37282742+shenyingkun@users.noreply.github.com>
2018-05-11 16:53:47 +0800

    Delete rman_backup.sh

--- rman_backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /home/oracle/.bash_profile
-/u01/app/oracle/product/11.2.0/dbhome_1/bin/rman target / nocatalog cmdfile=/u01/bin/rman_cmd  log=/backup_rman/rman_full_`date +%Y%m%d%H%M`.log
